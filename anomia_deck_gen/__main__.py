e1537090fd4b1e5ac52039f0c056406f0d95e7f8
ethilty <ethilty@crimson.ua.edu>
2022-09-23 18:10:57 -0500

    Card generator

--- anomia_deck_gen/__main__.py
+++ anomia_deck_gen/__main__.py
@@ -1,7 +1,11 @@
 import click
 import csv
+import os
+from PIL import Image, ImageFont
 from typing import List
+
 from .utility import SingleDispatchLoader
+from .generator import generate_deck
 
 category_loader = SingleDispatchLoader[List[str]]()
 
@@ -16,17 +20,27 @@ def load_csv(path: str):
         reader = csv.reader(file)
         return [row[0] for row in reader]
 
+def symbol_loader(path: str) -> List[Image.Image]:
+    return [Image.open(entry.path) for entry in os.scandir(path) if entry.is_file()]
+
 @click.command()
 @click.option("--category-file", type=click.Path(exists=True, dir_okay=False), required=True, help="File containing a list of categories")
 @click.option("--symbol-folder", type=click.Path(exists=True, file_okay=False), required=True, help="Folder containing set of card symbols")
+@click.option("--font-file", type=click.Path(exists=True, dir_okay=False), required=True, help="Font used to render card text")
 @click.option("--output-folder", type=click.Path(file_okay=False, writable=True), required=True, help="Folder to output card images to")
-def generate_cards(category_file, symbol_folder, output_folder):
+def anomia_deck_gen(category_file, symbol_folder, font_file, output_folder):
     # Load categories
     categories = category_loader(category_file)
     if categories == None:
         raise click.ClickException("Failed to load categories")
-    for category in categories:
-        print(category)
+    # Load symbols
+    symbols = symbol_loader(symbol_folder)
+    # Load font
+    font = ImageFont.FreeTypeFont(font_file, 50)
+    # Generate deck
+    cards = generate_deck(categories, symbols, font=font, size=(500, 800), symbol_size=(300, 300), margin=50)
+    for image in cards:
+        image.show()
 
 if __name__ == "__main__":
-    generate_cards()
+    anomia_deck_gen()
---
+++ anomia_deck_gen/generator.py
@@ -0,0 +1,41 @@
+from PIL import Image, ImageDraw, ImageFont
+from typing import List, Tuple
+from random import shuffle
+
+def create_text_image(text: str, font: ImageFont.ImageFont) -> Image.Image:
+    img = Image.new("RGBA", font.getsize(text), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    draw.text((0, 0), text, fill="black", font=font)
+    return img
+
+def generate_card(
+    category: str,
+    symbol: Image.Image,
+    size: Tuple[int, int],
+    margin: int,
+    font: ImageFont.ImageFont) -> Image.Image:
+
+    # Create blank image
+    img = Image.new("RGBA", size, "white")
+    # Paste symbol into image
+    img.paste(symbol, tuple(size[i]//2 - symbol.size[i]//2 for i in range(2)))
+    # Draw text onto image
+    text_img = create_text_image(category, font)
+    img.alpha_composite(text_img, (size[0]//2 - text_img.size[0]//2, size[1] - margin - text_img.size[1]))
+    # Flip the text and draw on top
+    flipped_text = text_img.transpose(Image.Transpose.ROTATE_180)
+    img.alpha_composite(flipped_text, (size[0]//2 - flipped_text.size[0]//2, margin))
+    return img
+
+def generate_deck(categories: List[str], symbols: List[Image.Image], symbol_size: Tuple[int, int], **kwargs) -> List[Image.Image]:
+    # Preprocess the symbols by normalizing their size
+    normalized_symbols = [symbol.copy() for symbol in symbols]
+    for symbol in normalized_symbols:
+        symbol.thumbnail(symbol_size)
+    # Create a shuffled copy of the categories
+    shuffled_categories = list(categories)
+    shuffle(shuffled_categories)
+    # Tag each category with a symbol
+    tagged_categories = [(category, normalized_symbols[i % len(symbols)]) for i, category in enumerate(shuffled_categories)]
+    # Generate card images
+    return [generate_card(category, symbol, **kwargs) for category, symbol in tagged_categories]
