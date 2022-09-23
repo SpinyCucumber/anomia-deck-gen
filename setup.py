78ab911ad109285937128157a911d4b4a3b2fe94
ethilty <ethilty@crimson.ua.edu>
2022-09-23 15:40:48 -0500

    Add click

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     packages=["anomia_deck_gen"],
     description="Generates decks of Anomia cards using lists of categories",
     install_requires=[
-        "Pillow==9.2.0"
+        "click==8.1.3",
+        "colorama==0.4.5",
+        "Pillow==9.2.0",
     ],
 )
